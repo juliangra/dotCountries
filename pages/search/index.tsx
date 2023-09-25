f8a3922dbd70db7b61c8bb89d27dc4521a02b9dc
Julian <juliangr@stud.ntnu.no>
2023-09-25 23:05:50 +0200

    fix: update broken imports

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,19 +1,14 @@
-import { useRouter } from 'next/dist/client/router'
-import React, { useEffect } from 'react'
-
-interface indexProps {
-  query: string
-}
-
-const index: React.FC<indexProps> = () => {
-  const router = useRouter()
-
-  useEffect(() => {
-    router.push('/')
-    return
-  }, [])
-
+const SearchIndex = () => {
   return <></>
 }
 
-export default index
+export const getServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: '/',
+      permanent: false
+    }
+  }
+}
+
+export default SearchIndex
